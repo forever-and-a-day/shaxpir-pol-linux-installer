e91870cc17069fbb53373c4b81bd243077d9e3f3
forever-and-a-day <foreverghmail@gmail.com>
2021-09-01 21:45:51 -0700

    First commit, added script and readme.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # shaxpir-pol-linux-installer
 Unofficial PlayOnLinux Installation Script for Shaxpir
+
+## Disclaimer
+This installer is unofficial, and is not endorsed, affiliated, or supported by Shaxpir in any way. The Shaxpir desktop application is proprietary software, and using it means you agree to its [Terms of Service](https://shaxpir.com/terms-of-service) and [Privacy Policy](https://shaxpir.com/privacy-policy). This work is done on my own free time out of personal interest and is not related to my employer. 
+
+## Features
+* Downloads and installs the Shaxpir application to your Linux computer
+* Gives you a hug
+
+## Required software
+* Playonlinux (https://www.playonlinux.com/en/download.html)
+(For Linux Mint, Choose Ubuntu, then download and open the .deb file)
+
+## How to install
+1. Open PlayOnLinux
+2. Open the "Tools" menu, and choose "Run a local script"
+3. Click, Next, then browse, and open pol_shaxpir.sh from where it was downloaded to. 
+4. Click next and check "I agree" to confirm the warnings. This script is open-source, and can be reviewed for malware if you wish (open pol_shaxpir.sh in a text editor and have a look!).
+5. Continue to follow the installer's instructions. Don't create a desktop shortcut when asked, and choose to **run Shaxpir** when asked. Not running Shaxpir from the installer may cause Shaxpir to crash when you open it in the future (You may close Shaxpir after it successfully opens for the first time.)
+
+## Contributions
+Contributions are likely unnecessary, but should the installer point to an outdated exe feel free to submit a pr or open an issue. 
---
+++ pol_shaxpir.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+# Date : (2021-08-26 9:30)
+# Last revision : 2021-08-26
+# Wine version used : 5.22
+# Distribution used to test : Linux Mint 20 Cinnamon
+# Author : CamazingMe
+# Licence : Retail
+# Only For : http://www.playonlinux.com
+#
+#  
+# CHANGELOG
+# [CamazingMe] v1
+# 
+# KNOWN ISSUES :
+#  - (2021-08-26) Window maximizing is weird. Rezize window manually.
+# NOTE: Based on: https://www.playonlinux.com/en/app-4-Steam.html
+
+ 
+  
+[ "$PLAYONLINUX" = "" ] && exit 0
+source "$PLAYONLINUX/lib/sources"
+  
+TITLE="Shaxpir"
+WINEVERSION="5.22"
+PREFIX="Shaxpir"
+  
+# Starting the script
+POL_SetupWindow_Init
+POL_SetupWindow_presentation "$TITLE" "Shaxpir" "https://shaxpir.com/" "CamazingMe" "$PREFIX"
+
+POL_System_SetArch "x64"
+
+POL_SetupWindow_free_presentation "$TITLE" "Welcome to the Shaxpir installation wizard."
+#POL_SetupWindow_message "$(eval_gettext 'This installer is unofficial and is not affiliated with Shaxpir, inc.')" "$TITLE"
+POL_SetupWindow_message "This installer is unofficial, not supported, and is not affiliated with Shaxpir, Inc. in any way. Use of Shaxpir is governed by its Terms of Use (https://shaxpir.com/terms-of-service) and Privacy Policy (https://shaxpir.com/privacy-policy)" "Disclaimer"
+
+POL_SetupWindow_message "To ensure that Shaxpir installs correctly and runs in the future, ensure you check the box to run Shaxpir after installation. If you don't, Shaxpir may crash and reqire reinstallation." "Important!"
+
+# Downloading wine if necessary and creating prefix
+POL_Wine_PrefixCreate "$WINEVERSION"
+  
+# Installing mandatory dependencies
+# POL_Wine_InstallFonts
+# POL_Call POL_Install_corefonts
+# POL_Function_FontsSmoothRGB
+  
+# Downloading latest version
+cd "$POL_USER_ROOT/wineprefix/$PREFIX/drive_c/"
+  
+cd "$POL_USER_ROOT/wineprefix/$PREFIX/drive_c/"
+POL_Download "https://s3.amazonaws.com/shaxpir-installers/install-shaxpir-4.5.12.exe"
+ 
+# Installing Shaxpir
+POL_Wine_WaitBefore "$TITLE"
+POL_Wine "install-shaxpir-4.5.12.exe"
+  
+# Make shortcut
+POL_Shortcut "Shaxpir.exe" "$TITLE" "" "" "Office;"
+  
+POL_SetupWindow_message "Shaxpir was successfully installed." "Installation Complete"
+
+POL_SetupWindow_Close
+exit 0
